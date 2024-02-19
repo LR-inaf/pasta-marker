a5befe4bdea4cf4184e21387dcde00eaafa6f574
Nicola Borghi <nicoborghi@outlook.it>
2024-02-19 17:12:58 +0100

    new version

---
+++ pastamarkers/__init__.py
@@ -0,0 +1,8 @@
+import os
+import pastamarkers
+
+__ROOT__    = os.path.dirname(pastamarkers.__file__)
+
+dir_data = __ROOT__ + "/data/"
+
+# "/mnt/c/Users/nicob/Desktop/pastamarkers/test/data/MPD/"
