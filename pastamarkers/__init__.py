3178069f4160b61e94af1d4b8145f05f400607a1
Luca Rosignoli <luca.rosignoli@inaf.it>
2025-03-26 18:16:40 +0100

    fix marker color, add salsa colormaps and add_parmesan function

--- pastamarkers/__init__.py
+++ pastamarkers/__init__.py
@@ -1,8 +1,27 @@
 import os
-import pastamarkers
+import pickle
+from collections import namedtuple
+from matplotlib.colors import LinearSegmentedColormap
+from .utils import *
 
-__ROOT__    = os.path.dirname(pastamarkers.__file__)
+__ROOT__ = os.path.dirname(__file__)
 
 dir_data = __ROOT__ + "/data/"
 
-# "/mnt/c/Users/nicob/Desktop/pastamarkers/test/data/MPD/"
+# loading _markers
+pasta_file_path = __ROOT__ + r"\pasta.pkl"
+with open(pasta_file_path, "rb") as pkl_file:
+    _markers = pickle.load(pkl_file)
+
+_Marker = namedtuple("Marker", [key for key in _markers.keys()])
+pasta = _Marker(*_markers.values())
+
+# loading salsa
+salsa_file_path = __ROOT__ + r"\salsa.pkl"
+with open(salsa_file_path, "rb") as pkl_file:
+    _cmaps = pickle.load(pkl_file)["colormaps"]
+
+_Cmaps = namedtuple("Cmaps", [key for key in _cmaps.keys()])
+salsa = _Cmaps(
+    *[LinearSegmentedColormap.from_list(key, val) for key, val in _cmaps.items()]
+)
