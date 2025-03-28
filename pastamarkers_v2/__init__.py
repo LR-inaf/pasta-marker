d120455c8b4c8d914c648f6fa74405a042aad266
giuliapapini <giulia.papini@inaf.it>
2025-03-28 12:11:46 +0100

    Colormap inverted in __init__ file

--- pastamarkers_v2/__init__.py
+++ pastamarkers_v2/__init__.py
@@ -2,14 +2,15 @@ import os
 import pickle
 from collections import namedtuple
 from matplotlib.colors import LinearSegmentedColormap
-from .utils import *
+from utils import *
+import numpy 
 
 __ROOT__ = os.path.dirname(__file__)
 
 dir_data = __ROOT__ + "/data/"
 
 # loading _markers
-pasta_file_path = __ROOT__ + r"\pasta.pkl"
+pasta_file_path = __ROOT__ + r"/pasta.pkl"
 with open(pasta_file_path, "rb") as pkl_file:
     _markers = pickle.load(pkl_file)
 
@@ -17,11 +18,12 @@ _Marker = namedtuple("Marker", [key for key in _markers.keys()])
 pasta = _Marker(*_markers.values())
 
 # loading salsa
-salsa_file_path = __ROOT__ + r"\salsa.pkl"
+salsa_file_path = __ROOT__ + r"/salsa.pkl"
 with open(salsa_file_path, "rb") as pkl_file:
     _cmaps = pickle.load(pkl_file)["colormaps"]
 
 _Cmaps = namedtuple("Cmaps", [key for key in _cmaps.keys()])
+
 salsa = _Cmaps(
-    *[LinearSegmentedColormap.from_list(key, val) for key, val in _cmaps.items()]
+    *[LinearSegmentedColormap.from_list(key, val[::-1]) for key, val in _cmaps.items()]
 )
